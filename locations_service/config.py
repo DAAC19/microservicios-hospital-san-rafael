2dd81b5fa3e5b4922d1687483f8d30d73e068043
Diego Alberto <diego190801@gmail.com>
2026-05-13 20:49:14 -0500

    gitignore actualizado

--- locations_service/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:4268@localhost:5432/microservices-hospital"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- metrics_service/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:ruffo2319.@localhost:5432/microservices-hospital"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
